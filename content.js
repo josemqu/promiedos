39612f5b64e35c3c6ffa1f4de748885e2b60f9db
josemqu <mailjmq@gmail.com>
2024-11-09 22:42:45 -0300

    prettier format

--- content.js
+++ content.js
@@ -1,59 +1,57 @@
 // console.log( 'content.js' );
 const scriptNames = [
-	"app.js"
-	// "modules/storage.js",
-]
+  "app.js",
+  // "modules/storage.js",
+];
 
-const styleNames = [
-	"./style/style.css",
-]
+const styleNames = ["./style/style.css"];
 
 const imageNames = [
-	"modules/utils/icons/up.svg",
-	"modules/utils/icons/equal.svg",
-	"modules/utils/icons/down.svg"
-]
+  "modules/utils/icons/up.svg",
+  "modules/utils/icons/equal.svg",
+  "modules/utils/icons/down.svg",
+];
 
-const div = document.createElement( 'div' );
+const div = document.createElement("div");
 div.innerText = chrome.runtime.id;
-div.classList.add( "chromeExtensionID" );
-div.style.display = 'none';
-( document.body || document.documentElement ).appendChild( div );
+div.classList.add("chromeExtensionID");
+div.style.display = "none";
+(document.body || document.documentElement).appendChild(div);
 
-setTimeout( scriptsLoad, 1000 );
-setTimeout( stylesLoad, 1000 );
-setTimeout( imagesLoad, 1000 );
+setTimeout(scriptsLoad, 1000);
+setTimeout(stylesLoad, 1000);
+setTimeout(imagesLoad, 1000);
 
 function scriptsLoad() {
-	scriptNames.map( scriptName => {
-		// console.log( "Se cargó el script " + scriptName );
-		let elem = document.createElement( 'script' );
-		elem.setAttribute( "src", chrome.runtime.getURL( scriptName ) );
-		// if ( scriptName == "app.js" ) elem.setAttribute( "type", "module" );
-		elem.setAttribute( "type", "module" );
-		elem.onload = () => elem.remove();
-		( document.head || document.documentElement ).appendChild( elem );
-	} )
+  scriptNames.map((scriptName) => {
+    // console.log( "Se cargó el script " + scriptName );
+    let elem = document.createElement("script");
+    elem.setAttribute("src", chrome.runtime.getURL(scriptName));
+    // if ( scriptName == "app.js" ) elem.setAttribute( "type", "module" );
+    elem.setAttribute("type", "module");
+    elem.onload = () => elem.remove();
+    (document.head || document.documentElement).appendChild(elem);
+  });
 }
 
 function stylesLoad() {
-	styleNames.map( styleName => {
-		// console.log( "Se cargó el script " + styleName );
-		let elem = document.createElement( 'style' );
-		elem.setAttribute( "src", chrome.runtime.getURL( styleName ) );
-		elem.onload = () => elem.remove();
-		( document.head || document.documentElement ).appendChild( elem );
-	} )
+  styleNames.map((styleName) => {
+    // console.log( "Se cargó el script " + styleName );
+    let elem = document.createElement("style");
+    elem.setAttribute("src", chrome.runtime.getURL(styleName));
+    elem.onload = () => elem.remove();
+    (document.head || document.documentElement).appendChild(elem);
+  });
 }
 
 function imagesLoad() {
-	imageNames.map( imageName => {
-		// console.log( "Se cargó el script " + styleName );
-		let elem = document.createElement( 'img' );
-		elem.setAttribute( "src", chrome.runtime.getURL( imageName ) );
-		elem.onload = () => elem.remove();
-		( document.head || document.documentElement ).appendChild( elem );
-	} )
+  imageNames.map((imageName) => {
+    // console.log( "Se cargó el script " + styleName );
+    let elem = document.createElement("img");
+    elem.setAttribute("src", chrome.runtime.getURL(imageName));
+    elem.onload = () => elem.remove();
+    (document.head || document.documentElement).appendChild(elem);
+  });
 }
 
 // chrome.runtime.onMessage.addListener( function( msg, sender, sendResponse ) {
@@ -69,7 +67,6 @@ function imagesLoad() {
 // }, function( response ) {
 // 	console.log( response );
 // } );
-
 
 // div.parentNode.removeChild( div );
 
